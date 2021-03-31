c6bd6ece0da9ca49ce10e3b89dfc7f78068ffe7b
Martin Cerny <martin.cerny@xbim.cz>
2021-03-31 15:40:59 +0200

    Fixed viewer on the front page, added invite

---
+++ generator/Xbim.SiteBuilder/Resources/js/invitation.js
@@ -0,0 +1,21 @@
+﻿(
+    function () {
+        var dismissed = localStorage.getItem('invitationDismissed') === 'true';
+        if (dismissed) {
+            var ago = Date.now() - parseInt(localStorage.getItem('invitationDismissedOn'));
+            if (ago < 7 * 24 * 60 * 60 * 1000) {
+                // dismissed less than a week ago, so don't bother
+                return;
+            }
+        }
+
+        setTimeout(function () {
+            $('#invitation').collapse('show');
+        }, 2000)
+        $('#btn-invitation-dismiss').click(function () {
+            $('#invitation').collapse('hide');
+            localStorage.setItem('invitationDismissed', 'true');
+            localStorage.setItem('invitationDismissedOn', Date.now());
+        })
+    }
+)();
